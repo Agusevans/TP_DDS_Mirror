01a1a8c4b7f319b6b049d7a27f3cb94f326e79d9
Luis Gomez <luisgomez5847@gmail.com>
2022-05-18 20:17:02 -0300

    Calculo de recorrido de trayecto

--- src/main/java/domain/Trayecto.java
+++ src/main/java/domain/Trayecto.java
@@ -6,4 +6,27 @@ public class Trayecto {
 
     List<Tramo> tramos;
 
+    public float calcularRecorrido(){
+        float total = 0f;
+
+        for (Tramo tramo:tramos) {
+            total += tramo.calcularTramo();
+        }
+        System.out.println("Recorrido Total: " + total);
+
+        return total;
+    }
+    public float calcularRecorridoPorSeccion(){
+        float total = 0f;
+
+        for (int i = 0; i <= tramos.size() ; i++) {
+            float j = this.tramos.get(i).calcularTramo();
+            total += j;
+            System.out.println("Recorrido seccion " + i + ": " + j);
+        }
+        System.out.println("Recorrido Total: " + total);
+
+        return total;
+    }
+
 }
