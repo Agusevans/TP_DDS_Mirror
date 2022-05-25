0ce9c92f8f6d5dd06d2b9db845adbcb614bfdbeb
unknown <fede-macias@hotmail.com>
2022-05-25 15:03:49 -0300

    Subo funcion recorrerTramo

--- src/main/java/domain/Tramo.java
+++ src/main/java/domain/Tramo.java
@@ -9,4 +9,12 @@ public class Tramo {
     Punto puntoFin;
     List<Miembro> compartidoPor;
 
+    public float calcularTramo(){
+        float totalDistancia = 0f;
+
+        totalDistancia = (float) Math.sqrt(Math.abs(Math.pow(puntoFin.longitud - puntoInicio.longitud, 2) - Math.pow(puntoFin.latitud - puntoInicio.latitud, 2)));
+
+        return totalDistancia;
+    }
+
 }
