5d92eca423a66dcb0465996997130e23788bb64c
EstebanQ99 <javiquintana99@hotmail.com>
2022-07-09 14:08:23 -0300

    email

--- src/main/java/domain/Organizacion/Comunicacion/Comunicacion.java
+++ src/main/java/domain/Organizacion/Comunicacion/Comunicacion.java
@@ -3,5 +3,5 @@ package domain.Organizacion.Comunicacion;
 import domain.Organizacion.AgenteTerritorial;
 
 public interface Comunicacion {
-    public void notificar(AgenteTerritorial agenteTerritorial);
+    public void notificar(AgenteTerritorial agenteTerritorial, String mensaje);
 }
--- src/main/java/domain/Organizacion/Comunicacion/Email.java
+++ src/main/java/domain/Organizacion/Comunicacion/Email.java
@@ -1,4 +1,52 @@
 package domain.Organizacion.Comunicacion;
 
-public class Email {
+import domain.Organizacion.AgenteTerritorial;
+import domain.Organizacion.Comunicacion.Comunicacion;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class Email implements Comunicacion {
+
+    public void notificar(AgenteTerritorial agenteTerritorial, String mensaje) {
+
+        Properties propiedad = new Properties();
+        propiedad.setProperty("mail.smtp.host", "smtp.gmail.com");
+        propiedad.setProperty("mail.smtp.starttls.enable", "true");
+        propiedad.setProperty("mail.smtp.port", "587");
+        propiedad.setProperty("mail.smtp.auth", "true");
+
+        Session sesion = Session.getDefaultInstance(propiedad);
+
+        String correoRemitente = "";
+        String contrasena = "";
+        String correoDestinatario = agenteTerritorial.getEmail();
+        String asunto = "";
+
+        MimeMessage mail = new MimeMessage(sesion);
+
+        try {
+            mail.setFrom(new InternetAddress(correoRemitente));
+            mail.addRecipient(Message.RecipientType.TO, new InternetAddress(correoDestinatario));
+            mail.setSubject(asunto);
+            mail.setText(mensaje);
+
+            Transport transporte = sesion.getTransport("smtp");
+            transporte.connect(correoRemitente, contrasena);
+            transporte.sendMessage(mail, mail.getRecipients(Message.RecipientType.TO));
+            transporte.close();
+
+            System.out.println("Correo Enviado\n");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
